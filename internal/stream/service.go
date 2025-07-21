3c8cf484ffcdfb9cb35f12d81044781a1d9ebb9e
chuong <thienchuong004@gmail.com>
2025-07-21 10:13:53 +0700

    update

--- internal/stream/service.go
+++ internal/stream/service.go
@@ -14,3 +14,43 @@ import (
 type Service struct {
 	proto.UnimplementedFileUploadServiceServer
 }
+
+func (s *Service) DownloadFile(req *proto.DownloadFileRequest, stream proto.FileUploadService_DownloadFileServer) error {
+	if req.GetName() == "" {
+		return status.Error(codes.InvalidArgument, "name is required")
+	}
+
+	// open file
+	file, err:= os.Open(req.Name)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return status.Error(codes.NotFound, "file not found")
+		}
+		return err
+	}
+	defer file.Close()
+
+	const bufferSize = 5*1024 // send in 5kb chunks
+	buff := make([]byte, bufferSize)
+
+	for {
+		bytesRead, err:= file.Read(buff)
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+        // stream to cliens
+		err:= stream.Send(&proto.DownloadFileResponse{Content: buff[:bytesRead]}) 
+		if err != nil {
+			return err
+		}
+	}
+
+
+}
+
+func (s *Service) UploadFile(stream proto.FileUploadService_UploadFileServer) error {
+	
+}
