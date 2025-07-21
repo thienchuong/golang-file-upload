ad2838468f7a25cda1e69276f59703ed0d30a926
chuong <thienchuong004@gmail.com>
2025-07-21 22:01:45 +0700

    update

--- internal/stream/service.go
+++ internal/stream/service.go
@@ -1,27 +1,27 @@
 package stream
 
-
 import (
 	"fmt"
 	"io"
 	"os"
+
 	"github.com/google/uuid"
+	"github.com/thienchuong/golang-file-upload/proto"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"github.com/thienchuong/golang-file-upload/proto"
 )
 
 type Service struct {
 	proto.UnimplementedFileUploadServiceServer
 }
 
-func (s *Service) DownloadFile(req *proto.DownloadFileRequest, stream proto.FileUploadService_DownloadFileServer) error {
+func (s Service) DownloadFile(req *proto.DownloadFileRequest, stream proto.FileUploadService_DownloadFileServer) error {
 	if req.GetName() == "" {
-		return status.Error(codes.InvalidArgument, "name is required")
+		return status.Error(codes.InvalidArgument, "filename cannot be empty")
 	}
 
 	// open file
-	file, err:= os.Open(req.Name)
+	file, err := os.Open(req.Name)
 	if err != nil {
 		if os.IsNotExist(err) {
 			return status.Error(codes.NotFound, "file not found")
@@ -30,27 +30,47 @@ func (s *Service) DownloadFile(req *proto.DownloadFileRequest, stream proto.File
 	}
 	defer file.Close()
 
-	const bufferSize = 5*1024 // send in 5kb chunks
+	const bufferSize = 5 * 1024 // send in 5KiB chunks
 	buff := make([]byte, bufferSize)
-
 	for {
-		bytesRead, err:= file.Read(buff)
+		// read bytes
+		bytesRead, err := file.Read(buff)
 		if err != nil {
 			if err == io.EOF {
-				return nil
+				return nil // end of file, close stream
 			}
 			return err
 		}
-        // stream to cliens
-		err:= stream.Send(&proto.DownloadFileResponse{Content: buff[:bytesRead]}) 
+
+		// stream bytes to client
+		err = stream.Send(&proto.DownloadFileResponse{Content: buff[:bytesRead]})
 		if err != nil {
 			return err
 		}
 	}
-
-
 }
 
 func (s *Service) UploadFile(stream proto.FileUploadService_UploadFileServer) error {
-	
+	// generate file name
+	fileName:= fmt.Sprintf("$s.png", uuid.New().String())
+
+	// create file
+	fo, err:= os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer fo.Close()
+
+	for {
+		res, err:= stream.Recv()
+		if err != nil {
+			if err == io.EOF{
+				return stream.SendAndClose(&proto.UploadFileResponse(&proto.UploadFileResponse{Name: fileName})
+			}
+			return err
+		}
+		if _, err:= fo.Write(res.Content); err != nil {
+			return fmt.Errorf("failed to write file: %v", err)
+		}
+	}
 }
